912ca91c9bc45c16dbca6cc19b6a02b4f083ffa3
vala2004 <alex.valcourt.caron@usherbrooke.ca>
2020-03-05 23:43:38 -0500

    -> Added capacity to geometry and simulation infos to be serialized

--- simulator/factory/common/common.py
+++ simulator/factory/common/common.py
@@ -3,9 +3,11 @@ from collections.abc import MutableMapping
 
 class AttributeAsDictClass(MutableMapping):
     def __init__(self, **kwargs):
+        self._valid_keys = [self._generate_attr_key(k) for k in kwargs.keys()]
         self.__dict__.update({self._generate_attr_key(k): v for k, v in kwargs.items()})
 
     def generate_new_key(self, key, value):
+        self._valid_keys.append(self._generate_attr_key(key))
         self.__dict__[self._generate_attr_key(key)] = value
 
     def __setitem__(self, key, value):
@@ -29,3 +31,10 @@ class AttributeAsDictClass(MutableMapping):
 
     def _generate_attr_key(self, key):
         return "_{}".format(key)
+
+    def pop(self, k):
+        self._valid_keys = list(filter(lambda key: not k == key, k))
+        return super().pop(k)
+
+    def __str__(self):
+        return str({k: self.__dict__[k] for k in self._valid_keys})
