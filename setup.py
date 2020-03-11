8a84f3b8a9fae6325ff8bc8c83fa82f33e102b82
vala2004 <alex.valcourt.caron@usherbrooke.ca>
2020-03-11 17:39:48 -0400

    -> Created setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='simulation_generator',
+    version='1.0.0',
+    packages=['test', 'test.helpers', 'scripts', 'external', 'external.qspace_sampler', 'external.qspace_sampler.visu',
+              'external.qspace_sampler.bases', 'external.qspace_sampler.bases.tests',
+              'external.qspace_sampler.sampling', 'simulator', 'simulator.runner', 'simulator.runner.impl',
+              'simulator.factory', 'simulator.factory.common', 'simulator.factory.geometry_factory',
+              'simulator.factory.geometry_factory.utils', 'simulator.factory.geometry_factory.features',
+              'simulator.factory.geometry_factory.features.ORM',
+              'simulator.factory.geometry_factory.features.ORM.Objects', 'simulator.factory.geometry_factory.handlers',
+              'simulator.factory.simulation_factory', 'simulator.factory.simulation_factory.helpers',
+              'simulator.factory.simulation_factory.handlers', 'simulator.factory.simulation_factory.parameters'],
+    url='',
+    license='',
+    author='avcaron',
+    author_email='',
+    description=''
+)
