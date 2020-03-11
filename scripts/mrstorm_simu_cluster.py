b4b14ed630bbf85b5c3f9baca31e0a28bfd264f3
vala2004 <alex.valcourt.caron@usherbrooke.ca>
2020-03-11 11:47:01 -0400

    -> Path typo fix

--- scripts/mrstorm_simu_cluster.py
+++ scripts/mrstorm_simu_cluster.py
@@ -542,7 +542,7 @@ def generate_datasets(args):
             json.dump(description, open(description_filename, "w+"))
             archive.addfile(tarfile.TarInfo("{}_description.json".format(sim_pre)), open(description_filename))
 
-        archive.add(join(node_sim_output, "simulations_outputs"), arcname="simulations_outputs")
+        archive.add(join(node_sim_output, "simulation_outputs"), arcname="simulation_outputs")
 
     copyfile(sim_archive, join(global_sim_output, "data_node{}.tar.gz".format(rank)))
 
