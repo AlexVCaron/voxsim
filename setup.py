671308755852e68badcd100ffbe2193bcc7af5e3
vala2004 <alex.valcourt.caron@usherbrooke.ca>
2020-03-13 00:26:42 -0400

    -> Added scripts to setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
 from setuptools import setup
+import glob
 
-setup(
-    name='simulation_generator',
-    version='1.0.0',
-    packages=['test', 'test.helpers', 'scripts', 'external', 'external.qspace_sampler', 'external.qspace_sampler.visu',
-              'external.qspace_sampler.bases', 'external.qspace_sampler.bases.tests',
-              'external.qspace_sampler.sampling', 'simulator', 'simulator.runner', 'simulator.runner.impl',
-              'simulator.factory', 'simulator.factory.common', 'simulator.factory.geometry_factory',
-              'simulator.factory.geometry_factory.utils', 'simulator.factory.geometry_factory.features',
-              'simulator.factory.geometry_factory.features.ORM',
-              'simulator.factory.geometry_factory.features.ORM.Objects', 'simulator.factory.geometry_factory.handlers',
-              'simulator.factory.simulation_factory', 'simulator.factory.simulation_factory.helpers',
-              'simulator.factory.simulation_factory.handlers', 'simulator.factory.simulation_factory.parameters'],
-    url='',
-    license='',
-    author='avcaron',
-    author_email='',
-    description='',
-    data_files=[('.', ["config.py", "config.json"])]
-)
+if __name__ == "__main__":
+    setup(
+        name='simulation_generator',
+        version='1.0.0',
+        packages=['test', 'test.helpers', 'external', 'external.qspace_sampler', 'external.qspace_sampler.visu',
+                  'external.qspace_sampler.bases', 'external.qspace_sampler.bases.tests',
+                  'external.qspace_sampler.sampling', 'simulator', 'simulator.runner', 'simulator.runner.impl',
+                  'simulator.factory', 'simulator.factory.common', 'simulator.factory.geometry_factory',
+                  'simulator.factory.geometry_factory.utils', 'simulator.factory.geometry_factory.features',
+                  'simulator.factory.geometry_factory.features.ORM',
+                  'simulator.factory.geometry_factory.features.ORM.Objects', 'simulator.factory.geometry_factory.handlers',
+                  'simulator.factory.simulation_factory', 'simulator.factory.simulation_factory.helpers',
+                  'simulator.factory.simulation_factory.handlers', 'simulator.factory.simulation_factory.parameters'],
+        url='',
+        license='',
+        author='avcaron',
+        author_email='',
+        description='',
+        data_files=[('.', ["config.py", "config.json"])],
+        scripts=glob.glob("scripts/*.py")
+    )
