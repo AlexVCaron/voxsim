8714a5eec87d1429d9967691078ed5e04304c7b2
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-06-29 15:43:28 -0400

    Typo

--- simulator/default.py
+++ simulator/default.py
@@ -1,5 +1,5 @@
 import typing
 
-SINGULARITY_PATH: typing.Final[str] = "utils"
+SINGULARITY_PATH: typing.Final[str] = "."
 SINGULARITY_NAME: typing.Final[str] = "voxsim_singularity_latest.sif"
 SINGULARITY_EXEC: typing.Final[str] = "singularity"
