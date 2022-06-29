e260acc3e306b7f4949c799bc638f896ecf31d65
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-06-29 13:22:12 -0400

    Added SingularityConfig class

---
+++ simulator/runner/config.py
@@ -0,0 +1,8 @@
+import dataclasses
+import simulator.default as default
+
+
+@dataclasses.dataclass
+class SingularityConfig:
+    singularity_path: str = default.SINGULARITY_PATH
+    singularity_name: str = default.SINGULARITY_NAME
