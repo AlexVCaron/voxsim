d0bb89a714fdb78fabdd45e9a86afd4069c39f23
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-06-29 16:07:00 -0400

    Bumped version

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ from setuptools import find_packages, setup
 
 from simulator.utils.setup.documentation import DocCommand
 
-
 if __name__ == "__main__":
     setup(
         name="simulation_generator",
-        version="1.0.0",
+        version="1.0.1",
         packages=find_packages(exclude=("tests", "tests.*")),
         url="",
         license="",
