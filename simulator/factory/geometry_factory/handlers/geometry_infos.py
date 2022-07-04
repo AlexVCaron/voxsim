09824b187a7eae76baaa248c593e07ad6f8f2534
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-07-04 18:27:46 -0400

    Replaced `file_path` and `base_file` for typed `file`

--- simulator/factory/geometry_factory/handlers/geometry_infos.py
+++ simulator/factory/geometry_factory/handlers/geometry_infos.py
@@ -1,28 +1,24 @@
 from ...common import AttributeAsDictClass
+import pathlib
 
 
 class GeometryInfos(AttributeAsDictClass):
     def __init__(
-        self, file_path, base_file, resolution, spacing, n_maps, **kwargs
+            self, file: pathlib.Path, resolution, spacing, n_maps, **kwargs
     ):
         super().__init__(**kwargs)
-        self.generate_new_key("file_path", file_path)
-        self.generate_new_key("base_file", base_file)
+        self.generate_new_key("file", file)
         self.generate_new_key("resolution", resolution)
         self.generate_new_key("spacing", spacing)
         self.generate_new_key("n_maps", n_maps)
 
-    def get_file_path(self):
-        return self._file_path
+    @property
+    def file(self) -> pathlib.Path:
+        return self._file
 
-    def set_file_path(self, path):
-        self._file_path = path
-
-    def get_base_file_name(self):
-        return self._base_file
-
-    def set_base_file_name(self, name):
-        self._base_file = name
+    @file.setter
+    def file(self, file: pathlib.Path):
+        self._file = file
 
     def get_resolution(self):
         return self._resolution
