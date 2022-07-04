d26f36764369290bfdfed566c39446d6c69970e1
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-07-04 18:40:59 -0400

    Type annotate paths in SimulationInfos

--- simulator/factory/simulation_factory/handlers/simulation_infos.py
+++ simulator/factory/simulation_factory/handlers/simulation_infos.py
@@ -1,23 +1,24 @@
 from ...common import AttributeAsDictClass
+import pathlib
 
 
 class SimulationInfos(AttributeAsDictClass):
-    def __init__(self, file_path, simulation_file_name, ids):
+    def __init__(self, file_path: pathlib.Path, simulation_file_name: pathlib.Path, ids):
         super().__init__()
         self.generate_new_key("file_path", file_path)
         self.generate_new_key("param_file", simulation_file_name)
         self.generate_new_key("compartment_ids", ids)
 
-    def get_file_path(self):
+    def get_file_path(self) -> pathlib.Path:
         return self._file_path
 
-    def set_file_path(self, file_path):
+    def set_file_path(self, file_path: pathlib.Path):
         self._file_path = file_path
 
-    def get_simulation_file_name(self):
+    def get_simulation_file_name(self) -> pathlib.Path:
         return self._param_file
 
-    def set_simulation_file_name(self, name):
+    def set_simulation_file_name(self, name: pathlib.Path):
         self._param_file = name
 
     @classmethod
