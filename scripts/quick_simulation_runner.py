ecb714508321b23571b7868fbec7ac0bfcd0402a
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-08-22 11:07:43 -0400

    Added quick geometry generation to test script

--- scripts/quick_simulation_runner.py
+++ scripts/quick_simulation_runner.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import math
 import pathlib
 import random
 import tempfile
@@ -8,10 +9,73 @@ import tempfile
 import numpy as np
 import numpy.linalg
 
-from scripts.geometry_factory import get_geometry_parameters
-from simulator.factory import SimulationFactory
+from simulator.factory import GeometryFactory, Plane, SimulationFactory
 from simulator.runner.legacy import SimulationRunner
 from simulator.utils.test_helpers import GeometryHelper
+
+resolution = [1, 1, 1]
+spacing = [1, 1, 1]
+
+spheres_center = [[-2, 7, 10], [2, -1, 11]]
+sphere_radius = 5
+
+n_point_per_centroid = 5
+bundle_radius = 4
+bundle_symmetry = 1
+bundle_n_fibers = 200
+bundle_limits = [[0, 1], [0, 1], [0, 1]]
+bundle_center = [0.5, 0.5, 0.5]
+world_center = [5, 5, 5]
+
+base_anchors = [
+    [0.5, -0.3, 0.5],
+    [0.5, -0.2, 0.5],
+    [0.5, -0.1, 0.5],
+    [0.5, 0, 0.5],
+    [0.5, 0.1, 0.5],
+    [0.5, 0.2, 0.5],
+    [0.5, 0.3, 0.5],
+    [0.5, 0.4, 0.5],
+    [0.5, 0.5, 0.5],
+    [0.5, 0.6, 0.5],
+    [0.5, 0.7, 0.5],
+    [0.5, 0.8, 0.5],
+    [0.5, 0.9, 0.5],
+    [0.5, 1.1, 0.5],
+    [0.5, 1.2, 0.5],
+    [0.5, 1.3, 0.5],
+]
+
+
+def get_geometry_parameters(output_folder: pathlib.Path, output_naming: str):
+    geometry_handler = GeometryFactory.get_geometry_handler(resolution, spacing)
+
+    bundle1 = GeometryFactory.create_bundle(
+        bundle_radius, bundle_symmetry, n_point_per_centroid, base_anchors
+    )
+    _, bundle2 = GeometryFactory.rotate_bundle(
+        bundle1, [0.5, 0.5, 0.5], math.pi / 6.0, Plane.YZ
+    )
+
+    cluster = GeometryFactory.create_cluster(
+        GeometryFactory.create_cluster_meta(
+            3, bundle_n_fibers, 1, bundle_center, bundle_limits
+        ),
+        [bundle1, bundle2],
+        world_center,
+    )
+
+    geometry_handler.add_cluster(cluster)
+
+    sphere_1 = GeometryFactory.create_sphere(sphere_radius, spheres_center[0])
+    sphere_2 = GeometryFactory.create_sphere(sphere_radius, spheres_center[1])
+
+    geometry_handler.add_sphere(sphere_1)
+    geometry_handler.add_sphere(sphere_2)
+
+    return geometry_handler.generate_json_configuration_files(
+        output_naming, output_folder
+    )
 
 
 def get_simulation_parameters(output_folder: pathlib.Path, output_naming: str):
