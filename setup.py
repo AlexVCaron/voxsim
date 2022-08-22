4342a41141a7bc171fea29cb25681cae3312441f
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-08-22 14:08:15 -0400

    Increased version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from simulator.utils.setup.documentation import DocCommand
 if __name__ == "__main__":
     setup(
         name="simulation_generator",
-        version="1.0.8",
+        version="1.0.9",
         packages=find_packages(exclude=("tests", "tests.*")),
         url="",
         license="",
